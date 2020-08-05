4ccc46d4f20c6fa276a0b7cb9fe979ab582d5b29
y1jay <67768207+y1jay@users.noreply.github.com>
2020-08-05 10:14:44 +0900

    add

---
+++ controllers/book_user.js
@@ -0,0 +1,51 @@
+const connection = require("../db/mysql_connection");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// @desc    회원가입
+// @route   POST /api/v1/book_user
+// @parameters  email, passwd
+exports.signUp = async (req, res, next) => {
+  let email = req.body.email;
+  let passwd = req.body.passwd;
+
+  const hashedPasswd = await bcrypt.hash(passwd, 8);
+
+  // 이메일이 정상적인가 체크
+  if (!validator.isEmail(email)) {
+    res.status(500).json({ success: false, message: "이게 이메일이냐?" });
+    return;
+  }
+  // 유저 인서트
+  let query = "insert into book_user (email, passwd) values ? ";
+  let data = [email, hashedPasswd];
+  let user_id;
+  try {
+    [result] = await connection.query(query, [[data]]);
+    user_id = result.insertId;
+  } catch (e) {
+    if (e.errno == 1062) {
+      // 이메일 중복된것 이다.
+      res
+        .status(400)
+        .json({ success: false, errno: 1, message: "이메일 중복" });
+      return;
+    } else {
+      res.status(500).json({ success: false, error: e });
+      return;
+    }
+  }
+
+  let token = jwt.sign({ user_id: user_id }, process.env.ACCESS_TOKEN_SECRET);
+
+  query = "insert into book_user_token (token, user_id) values ( ? , ? )";
+  data = [token, user_id];
+
+  try {
+    [result] = await connection.query(query, data);
+    res.status(200).json({ success: true, token: token });
+  } catch (e) {
+    res.status(500).json({ success: false, error: e });
+  }
+};
