5744cd2fc2f463fbfae763a733ac1bf812c272d8
y1jay <67768207+y1jay@users.noreply.github.com>
2020-08-05 10:21:32 +0900

    add

--- routes/book_user.js
+++ routes/book_user.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const auth = require("../middleware/auth");
-const { signUp } = require("../controllers/sns_users");
+// const auth = require("../middleware/auth");
+const { signUp } = require("../controllers/book_user");
 const router = express.Router();
 router.route("/").post(signUp);
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config;
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 app.use(express.json());
+
+const book_user = require("./routes/book_user");
+
+app.use("/api/v1/book_user", book_user);
 
 const PORT = process.env.PORT || 5700;
 
